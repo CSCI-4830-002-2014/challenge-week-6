c14a9cd91a4af2c0bfb2f7924eae3c02345e15e9
Alexander Tsankov <antsankov@gmail.com>
2014-10-03 13:54:34 -0600

    Almost done with challenge 3

--- mongodb4.js
+++ mongodb4.js
@@ -1,56 +1,60 @@
-var rest = require('restler');
+var data = require('./result7.json');
+
 var MongoClient = require('mongodb').MongoClient;
-var address = 'https://api.github.com/orgs/CSCI-4830-002-2014/events'
-var GitHubApi = require("github");
-var async = require("async");
-var pages; 
+
+//This is commented out for now because I am being rate limited by github. To run proeprly, uncomment and change hte source for data varaible
+// var GitHubApi = require("github");
+// var async = require("async");
+
+// var github = new GitHubApi({
+//     // required
+//     version: "3.0.0",
+//     // optional
+//     timeout: 5000
+// });
  
-var github = new GitHubApi({
-    // required
-    version: "3.0.0",
-    // optional
-    timeout: 5000
-});
+// function getClassEventsForPage(i, callback){
+//   github.repos.getFromOrg({
+//       org:"CSCI-4830-002-2014",
+//       page: i
+//   }, callback);
+// };
  
-function getClassEventsForPage(i, callback){
-  github.repos.getFromOrg({
-      org:"CSCI-4830-002-2014",
-      page: i
-  }, callback);
-};
+// // [ [a,a,a], [b,b,b], ... ] -> [a,a,a,b,b,b, ... ]
+// function flatten_fast(input){ 
+//     return input.reduce(function(a, b) {
+//       return a.concat(b);
+//   }, []);
+// }
+// async.map([1,2,3,4,5,6,7,8,9,10], getClassEventsForPage, function(err, results){
  
-function flatten_fast(input){ 
-    return input.reduce(function(a, b) {
-      return a.concat(b);
-  }, []);
-}
+//   flattened = flatten_fast(results);    
+  
+//   data = JSON.stringify(flattened, undefined, 10);
+
+// });
 
 MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
     if(err) throw err;
-  //this prints out the address of the website I am reading from. 
-  console.log("Getting info from address: " + address)
-  db.dropCollection('test_insert_github', function(err, result) {
-    console.dir(result);
-  });
-  
-  async.map([1,2,3,4,5,6,7,8,9,10], getClassEventsForPage, function(err, results){
  
-  flattened = flatten_fast(results);    
-  
-  pages = (JSON.stringify(flattened, undefined, 10));
-  }); 
+    console.log(">> Dropping collection");
+    console.log(data)
 
-
-  // Create a collection to store the results from github
-  var collection = db.collection('challenge_4');
-  collection.insert(data, function(err, docs) {
-  // Locate all the entries using find
-  collection.find().toArray(function(err, results) {
-    results.forEach(function(x){
-    console.log("name:" + x.actor.login + ", event type:" + x.type + ", repo " + x.repo.name);
+    db.dropCollection('test_insert_github', function(err, result) {
+      console.log("dropped: ");
+      console.dir(result);
+  });
+    // Create a collection to store the results from github
+    var collection = db.collection('challenge_4');
+    collection.insert(data, function(err, docs) {
+ 
+       // Locate all the entries using find
+      collection.find().toArray(function(err, results) {
+        results.forEach(function(x){
+          console.log("name: " + x.full_name + " --- id: " + x.owner.login);
         });
         // Let's close the db
         db.close();
       });       
     });
-});
+})
