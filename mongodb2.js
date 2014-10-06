62b97e04a75ff9c7784a3d3308a72483e0361227
Joshua Ferge <pawsh@joshuas-air.hsd1.co.comcast.net>
2014-10-05 23:13:19 -0600

    second commit

--- mongodb2.js
+++ mongodb2.js
@@ -10,10 +10,10 @@ MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
     console.dir(result);
   });
  
-  rest.get('https://api.github.com/orgs/csci-4830-002-2014/repos').on('complete', function(data) {
+  rest.get('https://api.github.com/orgs/CSCI-4830-002-2014/events').on('complete', function(data) {
  
     // Create a collection to store the results from github
-    var collection = db.collection('4830Repos');
+    var collection = db.collection('4830Events');
     collection.insert(data, function(err, docs) {
  
        // Locate all the entries using find
